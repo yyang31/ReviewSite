b4eefce9add6fa88348b7f49afb268574645e0b3
Youwei Yang <stanley316868@gmail.com>
2019-02-19 11:42:00 -0800

    implemented animated feature slide show

---
+++ js/mainJs.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  document.getElementById("pageLoader").style.visibility = "hidden";
+
+  // feature slide show timer
+  var slideTimer = setInterval(featureSlide, 2000);
+}
+
+function featureSlide() {
+  var featureCont = document.getElementsByClassName("feature-cont");
+
+  for(var i = 0; i < featureCont.length; i++){
+    if(featureCont[i].classList.contains("active")){
+      featureCont[i].classList.remove("active");
+
+      if((i+1) == featureCont.length){
+        featureCont[0].classList.add("active");
+      }else{
+        featureCont[i+1].classList.add("active");
+        break;
+      }
+    }
+  }
+}
