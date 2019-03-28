b1127c8e8c6dc01f30231dda47fa9277cd9bd488
Youwei Yang <stanley316868@gmail.com>
2019-03-28 11:05:02 -0700

    attempt to fix reactjs related issue

--- public/js/footerJs.js
+++
@@ -1,6 +0,0 @@
-/**
- * window.onload function - hides the page until everything is loaded
- */
-window.onload = function () {
-  document.getElementById("pageLoader").style.visibility = "hidden";
-}
